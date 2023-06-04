4e6bb92d008ce52e4a21558b34f78758bb43492f
chaoting <chaoting666@gmail.com>
2023-06-05 06:12:06 +0800

    docs: add README.md

--- script/DeployCompound.s.sol
+++ script/DeployCompound.s.sol
@@ -12,6 +12,20 @@ contract DeployCompound is Script {
 
     }
 
+// 1. 撰寫一個 Foundry 的 Script，該 Script 要能夠部署
+//     - 一個 CErc20Delegator(`CErc20Delegator.sol`，以下簡稱 cERC20)
+//     - 一個 Unitroller(`Unitroller.sol`)
+//     - 以及他們的 Implementation 合約
+//     - 和合約初始化時相關必要合約
+    
+//     請遵循以下細節：
+    
+//     - cERC20 的 decimals 皆為 18
+//     - 自行部署一個 cERC20 的 underlying ERC20 token，decimals 為 18
+//     - 使用 `SimplePriceOracle` 作為 Oracle
+//     - 使用 `WhitePaperInterestRateModel` 作為利率模型，利率模型合約中的借貸利率設定為 0%
+//     - 初始 exchangeRate 為 1:1
+
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
   
