840ca78752ac4f52353fa5d78358d5a5b2821227
chaoting <chaoting666@gmail.com>
2023-06-05 05:54:40 +0800

    chore: install openzeppelin

--- script/DeployCompound.s.sol
+++ script/DeployCompound.s.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "compound-protocol/contracts/Unitroller.sol";
 import "compound-protocol/contracts/Comptroller.sol";
+// import { CErc20 } from "compound-protocol/contracts/CErc20.sol";
+import "openzeppelin/token/ERC20/ERC20.sol";
 
 contract DeployCompound is Script {
     function setUp() public {
